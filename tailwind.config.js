12a23c02038ff084d5acdc7e8ed9310fe7df522c
socratesssss <studentidsijan@gmail.com>
2024-11-05 21:19:33 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./output/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
